b7f6f550713667df910d052f01cc858e4adfba32
akisuperprof <aki.superprof@gmail.com>
2026-07-14 18:26:57 +0900

    SECURITY: Remove temp debug scripts to fix ESLint

--- test-json.js
+++
@@ -1,8 +0,0 @@
-const { NextResponse } = require('next/server');
-try {
-  let a = {};
-  a.b = a;
-  JSON.stringify(a);
-} catch(e) {
-  console.log(e.message);
-}
--- test-run-api.js
+++
@@ -1,4 +0,0 @@
-const { POST } = require('./.next/server/app/api/admin/generation-jobs/run/route.js');
-const { createRequest } = require('node-mocks-http'); // If we don't have it, we'll just mock it manually
-
-// Actually, calling the compiled Next.js output might be tricky. Let's just use ts-node to execute the route directly, or compile a test.
